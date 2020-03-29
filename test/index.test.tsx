497ee1c648025854812f7b511c693902575dcc08
Ixl123 <cofman.jacob@gmx.de>
2020-03-29 17:23:21 +0200

    chore: fix release

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import { Thing } from '../src';
+// import useCSSCustomProperty from '../src';
 
 describe('it', () => {
-  it.todo('renders without crashing', () => {
+  it('renders without crashing', () => {
     const div = document.createElement('div');
-    // ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<div>test</div>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
